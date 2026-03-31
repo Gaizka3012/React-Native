b023ee8b6bfca4611b06b23ec26728499ebf2996
Gaizka3012 <gaizka0010@gmail.com>
2026-03-31 16:53:26 +0200

    Componentes funcionales en React Native

--- appGaztaroa/componentes/CampobaseComponent.js
+++ appGaztaroa/componentes/CampobaseComponent.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import Calendario from './CalendarioComponent';
 import { EXCURSIONES } from '../comun/excursiones';
-class Campobase extends Component {
- constructor(props) {
- super(props);
- this.state = {
- excursiones: EXCURSIONES
- };
- }
- render() {
+import DetalleExcursion from './DetalleExcursionComponent';
+import { View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
- return (
- <Calendario excursiones={this.state.excursiones} />
- );
- }
+class Campobase extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            excursiones: EXCURSIONES,
+            seleccionExcursion: null
+        };
+    }
+    onSeleccionExcursion(excursionId) {
+        this.setState({ seleccionExcursion: excursionId })
+    }
+    render() {
+
+        return (
+            <SafeAreaView style={{ flex: 1 }}>
+                <DetalleExcursion excursion={this.state.excursiones.filter((excursion) => excursion.id === this.state.seleccionExcursion)[0]} />
+                <Calendario excursiones={this.state.excursiones} onPress={(excursionId) => this.onSeleccionExcursion(excursionId)} />
+            </SafeAreaView>
+        );
+    }
 }
 export default Campobase; 
