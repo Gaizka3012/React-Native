050feddfac35b455e8e415210f2965516a9ca1d4
Gaizka3012 <gaizka0010@gmail.com>
2026-04-11 14:21:24 +0200

    Commit 04: “Stack Navigation”

--- appGaztaroa/App.js
+++ appGaztaroa/App.js
@@ -10,19 +10,10 @@ export default function App() {
     <SafeAreaProvider>
       <PaperProvider>
         <View style={{ flex: 1 }}>
-          <Campobase />
+          <Campobase/>
           <StatusBar style="auto" />
         </View>
       </PaperProvider>
     </SafeAreaProvider>
   );
 }
-
-/* const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-}); */
